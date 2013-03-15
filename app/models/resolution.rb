1fd7842c8e8e309ab669f31a304462ccd8229d58
Jasper Fu <jian.fu@gree.co.jp>
2013-03-15 15:31:08 +0800

    optimize views

--- app/models/resolution.rb
+++ app/models/resolution.rb
@@ -1,9 +1,7 @@
 class Resolution < ActiveRecord::Base
   attr_accessible :resolution
-  validates_uniqueness_of :resolution
-  validate :resolution_format
-
-  def resolution_format
-    errors.add :resolution, "Invalid format, please use format like 400x300!" unless resolution =~ /\d+x\d+/
-  end
+  validates :resolution,
+            :format => { :with => /\d+x\d+/, :message => "Invalid format, please use format like 400x300!" },
+            :uniqueness => true,
+            :presence => true
 end
